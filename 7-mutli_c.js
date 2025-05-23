e61f8b19a72acf933b12bbb3a4cb38e4c2a6154e
LungileK <llungilekhoza@gmail.com>
2025-05-23 22:48:55 +0200

    Added 6-multi_languages_loop.js,7-multi_c.js, 8-square.js, 9-add.js,  10-factorial.js

---
+++ 7-mutli_c.js
@@ -0,0 +1,9 @@
+const count = paseInt(ProcessingInstruction.argv[2]);
+
+if (isNaN(count)) {
+    console.log("Missing number of occurences");
+} else {
+    for (let i = 0; i < count; i++) {
+        console.log("C is fun");
+    }
+}
---
+++ 9-add.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+    console.log(a +b);
+}
+
+const a = parseInt(ProcessingInstruction.argv[2]);
+const b = parseInt(ProcessingInstruction.argv[3]);
+
+add(a, b);
